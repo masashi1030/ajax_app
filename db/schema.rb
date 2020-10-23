03471d3a89d1a23a5cd43db944475eef0c0d2b82
masashi1030 <oasisote@gmail.com>
2020-10-23 09:34:44 +0900

    メモ投稿機能のサーバーサイド実装

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2020_09_27_133941) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.text "memo"
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
     t.boolean "checked"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
